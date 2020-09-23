6afef2543f005571dd669369aa3bd8368c7417e4
ohmori <kirio9817@gmail.com>
2020-09-23 09:40:34 +0900

    [add] get_emotion.js #4

---
+++ electron/src/get_emotion.js
@@ -0,0 +1,59 @@
+Promise.all([
+    // faceapi.nets.tinyFaceDetector.loadFromUri('/models'), //light model for face detection
+    faceapi.nets.ssdMobilenetv1.loadFromUri('./models'), //heavy model for face detection
+    faceapi.nets.faceExpressionNet.loadFromUri('./models')// for emotion recognition
+])
+
+
+class Emotion {
+    constructor() {
+        // count: the number of times when there are no people
+        this.count = 0
+        this.pre_emotion = 'neural'
+    }
+    // get the most probable key
+    maxIndex(expressions) {
+        let indexs = Object.keys(expressions)
+        let index = indexs[0]
+        let value = -Infinity
+        for (let i = 0, l = indexs.length; i < l; i++) {
+            if (value < expressions[indexs[i]]) {
+                value = expressions[indexs[i]]
+                index = indexs[i]
+            }
+        }
+        return index
+    }
+    get_emotion(detections) {
+        let emotion
+        if (detections[0]) {
+            // if detect people
+            this.count = 0
+            emotion = this.maxIndex(detections[0]["expressions"])
+            console.log(emotion)
+
+            // threshold of emotion 
+            if(detections[0]["expressions"][emotion] < 0.8) {
+                console.log("pre_emotion")
+                emotion = this.pre_emotion
+            }
+
+            this.pre_emotion = emotion
+        } else {
+            // if don't detect people
+            this.count += 1
+            if(this.count > 5) {
+                // the case that cannot detect more than 5 times in a row
+                console.log("absent")
+                return "absent"
+            }
+            console.log("pre_emotion")
+            emotion = this.pre_emotion
+            console.log(this.pre_emotion)
+        }
+
+        return emotion
+    }
+}
+
+
--- electron/src/recog.js
+++ electron/src/recog.js
@@ -9,46 +9,28 @@ Promise.all([
 
 var video = document.getElementById("video");
 
-var pre_emotion = 'neural'
 
 var media = navigator.mediaDevices.getUserMedia({
     video: true,//get video
     audio: false,//do not get audio
 });
 
-// get the most probable key
-function maxIndex(a) {
-    let indexs = Object.keys(a)
-    let index = indexs[0]
-    let value = -Infinity
-    for (let i = 0, l = indexs.length; i < l; i++) {
-        if (value < a[indexs[i]]) {
-            value = a[indexs[i]]
-            index = indexs[i]
-        }
-    }
-    return index
-}
 
+// Event for get emotion
 video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
     const displaySize = { width: video.width, height: video.height }
     faceapi.matchDimensions(canvas, displaySize)
+
+    recog_emotion = new Emotion()
+
     setInterval(async () => {
+        //get face positions and probability of emotions
         const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options()).withFaceExpressions()
         const resizedDetections = faceapi.resizeResults(detections, displaySize)
 
-        //やるならここをクラスに　detections[0]を引数として感情(string)を返すプログラム 人が長時間検出できなければabsent
-        if (detections[0]) {
-            let emotion = maxIndex(detections[0]["expressions"])
-            console.log(emotion)
-            pre_emotion = emotion
-        } else {
-            console.log("pre_emotion")
-            emotion = pre_emotion
-            console.log(pre_emotion)
-        }
+        let emotion = recog_emotion.get_emotion(detections)
 
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
         faceapi.draw.drawDetections(canvas, resizedDetections)
