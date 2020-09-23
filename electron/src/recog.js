98e268541609c6185d11ae0f8232d2c6f28b5b88
choron634 <h.syou94@gmail.com>
2020-09-23 13:49:01 +0900

    sho: camera select ok #16

--- electron/src/recog.js
+++ electron/src/recog.js
@@ -1,21 +1,29 @@
-// var faceapi = require('./face-api.min.js')
-
-
 Promise.all([
     // faceapi.nets.tinyFaceDetector.loadFromUri('/models'), //light model for face detection
     faceapi.nets.ssdMobilenetv1.loadFromUri('./models'), //heavy model for face detection
     faceapi.nets.faceExpressionNet.loadFromUri('./models')// for emotion recognition
 ])
 
-
-var video = document.getElementById("video");
-
 const cameraDeviceIds = [/* { deviceId, label } */];
 
-new Promise((resolve,reject)=>{
-    var cameraDeviceIds = select_cam();
-    resolve(cameraDeviceIds);
-}).then((cameraDeviceIds)=>{
+navigator.mediaDevices.enumerateDevices().then(function(mediaDevices) {
+    for (let len = mediaDevices.length, i = 0; i < len; i++) {
+        const item = mediaDevices[i];
+        // カメラデバイスの場合、 kind プロパティには "videoinput" が入っている:
+        if (item.kind === "videoinput") {
+        const deviceId = item.deviceId;
+        const label = item.label;
+        // デバイスIDとラベル変数に保存
+        cameraDeviceIds.push({ deviceId, label });
+        }
+    }
+    //console.log("cameras:"+JSON.stringify(cameraDeviceIds));
+}).then(result=>{
+
+    var video = document.getElementById("video");
+
+    //console.log("cameras:"+JSON.stringify(cameraDeviceIds[3].deviceId));
+
     var media = navigator.mediaDevices.getUserMedia({
         video: { deviceId: cameraDeviceIds[3].deviceId},//get video
         audio: false,//do not get audio
@@ -26,24 +34,25 @@ new Promise((resolve,reject)=>{
         document.body.append(canvas)
         const displaySize = { width: video.width, height: video.height }
         faceapi.matchDimensions(canvas, displaySize)
-
+    
         recog_emotion = new Emotion()
-
+    
         setInterval(async () => {
             //get face positions and probability of emotions
             const detections = await faceapi.detectAllFaces(video, new faceapi.SsdMobilenetv1Options()).withFaceExpressions()
             const resizedDetections = faceapi.resizeResults(detections, displaySize)
-
+    
             let emotion = recog_emotion.get_emotion(detections)
-
+    
             canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
             faceapi.draw.drawDetections(canvas, resizedDetections)
             faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
         }, 1000)
     })
-
+    
     // リアルタイムに再生（ストリーミング）させるためにビデオタグに流し込む
     media.then((stream) => {
         video.srcObject = stream;
     });
+    
 })
